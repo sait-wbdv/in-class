51f4fca939178312d5555c240bc896969629adbd
Tony Grimes <acidtone@tonygrimes.com>
2020-10-20 10:04:55 -0600

    added W7M and T

---
+++ week07t/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 100; i++) {
+  // If i is divisible by 3, log "Fizz"
+
+  // If i is divisible by 5, log "Buzz"
+
+  // If i is divisible by both 3 and 5, log "FizzBuzz"
+
+  // Otherwise, print the number
+  console.log(i);
+
+}
