ce1f96ee7d39da7c1cf8476ddefefc71d537739f
Tony Grimes <acidtone@tonygrimes.com>
2020-10-21 14:22:35 -0600

    added end of day in class

--- week07w/loop-find/js/animals.js
+++ week07w/loop-find/js/animals.js
@@ -1,89 +1,89 @@
 // Attibutions:
 // Lorem Picsum
 // https://picsum.photos/
-export const animals = {
-  237: {
+export const animals = [
+  {
     id:     237,
     title:  'Puppy',
     credit: 'André Spieker',
     url:    'https://unsplash.com/photos/8wTPqxlnKM4'
   },
-  433: {
+  {
     id:     433,
     title:  'Bear',
     credit: 'Thomas Lefebvre',
     url:    'https://unsplash.com/photos/aRXPJnXQ9lU'
   },
-  577: {
+  {
     id:     577,
     title:  'Moose',
     credit: 'Thomas Lefebvre',
     url:    'https://unsplash.com/photos/34mvN5nt1ls'
   },
-  582: {
+  {
     id:     582,
     title:  'Coyote',
     credit: 'Levi Saunders',
     url:    'https://unsplash.com/photos/NUMlxTPsznM'
   },
-  593: {
+  {
     id:     593,
     title:  'Tiger',
     credit: 'Paula Borowska',
     url:    'https://unsplash.com/photos/OM7CvKnhjfs'
   },
-  659: {
+  {
     id:     659,
     title:  'Husky',
     credit: 'Alexander Dimitrov',
     url:    'https://unsplash.com/photos/KGy1GFeKBu4'
   },
-  718: {
+  {
     id:     718,
     title:  'Wolf',
     credit: 'Josh Felise',
     url:    'https://unsplash.com/photos/mblYxasm0nk'
   },
-  783: {
+  {
     id:     783,
     title:  'Monkey',
     credit: 'Damir Kotorić',
     url:    'https://unsplash.com/photos/cSQw4uqF_Hc'
   },
-  790: {
+  {
     id:     790,
     title:  'Elk',
     credit: 'rebecca johnston',
     url:    'https://unsplash.com/photos/nuRYwOJryyk'
   },
-  837: {
+  {
     id:     837,
     title:  'Bulldog',
     credit: 'Yamon Figurs',
     url:    'https://unsplash.com/photos/2zlxFmByClI'
   },
-  1024: {
+  {
     id:     1024,
     title:  'Bird',
     credit: 'Мартин Тасев',
     url:    'https://unsplash.com/photos/7ALI0RYyq6s'
   },
-  1025: {
+  {
     id:     1025,
     title:  'Pug',
     credit: 'Matthew Henry',
     url:    'https://unsplash.com/photos/U5rMrSI7Pn4'
   },
-  1074: {
+  {
     id:     1074,
     title:  'Big Cat',
     credit: 'Samuel Scrimshaw',
     url:    'https://unsplash.com/photos/sseiVD2XsOk'
   },
-  1084: {
+  {
     id:     1084,
     title:  'Walrus',
     credit: 'Jay Ruzesky',
     url:    'https://unsplash.com/photos/h13Y8vyIXNU'
   }
-};
+];
