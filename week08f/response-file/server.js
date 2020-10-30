60186a4d8738e46a97a45124218e56d479d61d78
Tony Grimes <acidtone@tonygrimes.com>
2020-10-30 10:57:00 -0600

    added mid-morning in-class

---
+++ week08f/response-file/server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer(function(request, response) {
+  let json = "../assets/definitions.json";
+
+  fs.readFile(json, function(error, data) {
+    if (error) {
+      response.writeHead(404, { "Content-type": "text/html" });
+      response.end("<h1>No such image</h1>");
+    } else {
+      //specify the content type in the response will be an image
+      response.writeHead(200, { "Content-type": "application/json" });
+      response.end(data);
+    }
+  });
+});
+server.listen(8080, function() {
+  console.log("Server running on port 8080");
+});
