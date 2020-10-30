6021f8a1a003099d70e23ee104f7ba129d6b8493
Tony Grimes <acidtone@tonygrimes.com>
2020-10-30 13:10:49 -0600

    end-of-day

---
+++ week08f/endpoint-param/server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+
+const server = http.createServer(function(request,response) {
+
+  response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+
+  console.log(request.url);
+  
+  const params = request.url.split('/');
+  console.log(params);
+
+  // Pull in the :id
+  const photoId = params[2];
+
+  console.log(photoId);
+  console.log(parseInt(photoId));
+  console.log(Number.isInteger(parseInt(photoId)));
+
+  if (params[1] === 'photos' && Number.isInteger(parseInt(params[2]))) {
+
+    response.end(`<img src="https://picsum.photos/id/${photoId}/500/500/" alt="Lorem Picsum">`);
+
+  } else {
+    // Use the `request.url` property to gain access to the endpoint being requested. 
+    response.end(`<h1>Request URL: ${request.url}</h1>`);
+  }
+
+});
+
+server.listen(8080, function() {
+    console.log(`Listening on port 8080`);
+});
