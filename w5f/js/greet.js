349c4ea2b4d16e879521739c31d7ab96983d62de
Tony Grimes <acidtone@tonygrimes.com>
2021-02-12 10:39:29 -0700

    added mid-morning

---
+++ w5f/js/greet.js
@@ -0,0 +1,47 @@
+let firstName;
+let lastName;
+let salutation = 'Mx.';
+let protocol;
+
+// ES5
+// console.log('Hello, ' + firstName + ' ' + lastName + '.'); 
+
+
+// Check for undefined variables -> set defaults
+if (typeof firstName === 'undefined') {
+  // the variable is defined
+  firstName = 'John'
+}
+if (typeof lastName === 'undefined') {
+  // the variable is defined
+  lastName = 'Wick'
+}
+
+// If no salutation -> death!
+if (typeof salutation === 'undefined') {
+  // the variable is defined
+  protocol = 'battle';
+} 
+
+
+if(protocol === 'formal') {
+
+  // Formal greeting
+  console.log(`Hello, ${salutation} ${lastName}.`);
+
+} else if (protocol === 'informal') {
+
+  // Informal greeting
+  console.log(`Hi, ${firstName}.`);
+
+} else if (protocol === 'battle') {
+
+  // Battle cry
+  console.log(`${lastName}! You didn't fill out the form correctly! Prepare to die!`);
+
+} else {
+  
+  // Throw error
+  console.error(`Unrecognized protocol.`);
+
+}
