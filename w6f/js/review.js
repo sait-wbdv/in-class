bb3b944ae2aed766b1a40e19d98a3d382fba6dee
Tony Grimes <acidtone@tonygrimes.com>
2021-02-19 10:51:41 -0700

    added end-of morning

---
+++ w6f/js/review.js
@@ -0,0 +1,12 @@
+const string = 'some string';
+
+console.log(string);
+
+const price = 19.99;
+
+console.log(Math.floor(price));
+console.log(Math.ceil(price));
+
+// console.log(document);
+
+console.log(global);
